1bf8f5d3d0dcd69e3e65e002d50613e2e5204cce
Shahryar <dev.shahryar@gmail.com>
2026-06-07 09:05:52 -0400

    feat: set cookie for theme and add two pages

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -5,8 +5,21 @@ const light = document.getElementById("theme_light")
 const dark = document.getElementById("theme_dark")
 const system = document.getElementById("theme_system")
 
+
+function setCookie(name, value, days) {
+    const d = new Date()
+    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000))
+    document.cookie = name + "=" + value + ";expires=" + d.toUTCString() + ";path=/"
+}
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`
+    const parts = value.split(`; ${name}=`)
+    if (parts.length === 2) return parts.pop().split(';').shift()
+}
+
 function systemTheme() {
-    const darkModeMql = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)');
+    const darkModeMql = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)')
     if (darkModeMql && darkModeMql.matches) {
         return "dark"
     } else {
@@ -14,7 +27,8 @@ function systemTheme() {
     }
 }
 
-html.dataset.theme = systemTheme()
+const savedTheme = getCookie("theme")
+html.dataset.theme = savedTheme || systemTheme()
 
 let menuStatus = bg.dataset.show
 
@@ -28,10 +42,13 @@ bg.addEventListener("click" , function () {
 
 light.addEventListener("click" , function () {
     html.dataset.theme = "light"
+    setCookie("theme", "light", 365);
 })
 dark.addEventListener("click" , function () {
     html.dataset.theme = "dark"
+    setCookie("theme", "dark", 365);
 })
 system.addEventListener("click" , function () {
    html.dataset.theme = systemTheme()
+   setCookie("theme", "", -1);
 })
